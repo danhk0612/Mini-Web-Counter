cec4e07cd531527ef57054a4e417b03c2ba60ee7
HAEKWANG DAN <danhk0612@gmail.com>
2026-09-04 13:10:00 +0900

    Add configurable monitoring item model

---
+++ Models/MonitoringItem.cs
@@ -0,0 +1,9 @@
+namespace JCMS_Mini_Monitoring.Models;
+
+public sealed class MonitoringItem
+{
+    public string ValueName { get; set; } = "value";
+    public string BackgroundColor { get; set; } = "#666666";
+    public string TextColor { get; set; } = "#FFFFFF";
+    public bool Visible { get; set; } = true;
+}
