713ca7f19fd7541ba41c1a39ed2d0137dbbbe231
HAEKWANG DAN <danhk0612@gmail.com>
2026-09-04 13:10:11 +0900

    Support dynamic status values

--- Models/StatusData.cs
+++ Models/StatusData.cs
@@ -1,21 +1,16 @@
-using System.Text.Json.Serialization;
-
 namespace JCMS_Mini_Monitoring.Models;
 
 public sealed class StatusData
 {
-    [JsonPropertyName("fire")]
-    public int Fire { get; set; }
+    private readonly Dictionary<string, decimal> _values = new(StringComparer.Ordinal);
 
-    [JsonPropertyName("facility")]
-    public int Facility { get; set; }
+    public void SetValue(string name, decimal value)
+    {
+        _values[name] = value;
+    }
 
-    [JsonPropertyName("fault")]
-    public int Fault { get; set; }
-
-    [JsonPropertyName("block")]
-    public int Block { get; set; }
-
-    [JsonPropertyName("spare")]
-    public int Spare { get; set; }
+    public bool TryGetValue(string name, out decimal value)
+    {
+        return _values.TryGetValue(name, out value);
+    }
 }
